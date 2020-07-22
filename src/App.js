f0f973447c2a93ae6cd9492865bff5d43e502eb1
Moises Valdez <moisesvaldez075@gmail.com>
2020-07-22 11:51:29 -0700

    broken our homepage into small components, finished style out menu-item component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
-import HomePage from './homepage.component.jsx';
+import React, { Component } from 'react';
+import HomePage from './pages/homepage/homepage.compent.jsx';
 
-function App() {
-    return (
-        <div>
-            <HomePage />
-        </div>
-    );
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <HomePage />
+            </div>
+        );
+    }
 }
 
 export default App;
