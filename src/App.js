a4a59574785a428f1428c07412103cd27c79fc19
Moises Valdez <moisesvaldez075@gmail.com>
2020-08-04 12:51:10 -0700

    added firebase auth into the app

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.compent.jsx';
 import ShopPage from './pages/shop/shop.component';
+import ContactPage from './pages/contact/contact.component';
+import SignInAndSignUp from './pages/sign-in-and-sign-up/sing-in-and-sign-up.component';
 import Header from './components/header/header.component';
 
 const App = () => {
     return (
-        <div>
+        <Fragment>
             <Header />
             <Switch>
-                <Route exact path='/' component={HomePage} />
-                <Route path='/shop' component={ShopPage} />
-            </Switch>
-        </div>
+                <Route exact path='/' component={HomePage} />{' '}
+                <Route path='/shop' component={ShopPage} />{' '}
+                <Route path='/contact' component={ContactPage} />{' '}
+                <Route path='/signIn' component={SignInAndSignUp} />{' '}
+            </Switch>{' '}
+        </Fragment>
     );
 };
 
---
+++ src/pages/contact/contact.component.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import './contact.style.scss';
+
+const ContactPage = () => <h1>Contact Page</h1>;
+
+export default ContactPage;
